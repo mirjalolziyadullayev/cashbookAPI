7033becc78c5c470476c156e31c8b21021a8a367
Mirjalol Ziyadullayev <121923945+mirjalolziyadullayev@users.noreply.github.com>
2023-11-05 23:06:26 +0500

    minor improvements

--- handlers/transactions.go
+++ handlers/transactions.go
@@ -1,7 +1,10 @@
 package handlers
 
 import (
+	"cashbookAPI/models"
+	"encoding/json"
 	"net/http"
+	"os"
 )
 
 func TransactionHandler(w http.ResponseWriter, r *http.Request) {
@@ -18,9 +21,30 @@ func TransactionHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func getTransactions(w http.ResponseWriter, r *http.Request) {
+	var allUser []models.User
+	userByte, _ := os.ReadFile("db/all.json")
+	json.Unmarshal(userByte, &allUser)
 
+	var Transactions []models.Transactions
+
+	for i := 0; i < len(allUser); i++ {
+		for j := 0; j < len(allUser[i].Account); j++ {
+			for k := 0; k < len(allUser[i].Account[j].Transactions); k++ {
+				Transactions = append(Transactions, allUser[i].Account[j].Transactions[k])
+			}
+		}
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(Transactions)
 }
 func createTransaction(w http.ResponseWriter, r *http.Request) {
+	var newTransaction models.Transactions
+	json.NewDecoder(r.Body).Decode(&newTransaction)
+
+	var allUser []models.User
+	userByte, _ := os.ReadFile("db/all.jon")
+	json.Unmarshal(userByte, &allUser)
 
 }
 func updateTransaction(w http.ResponseWriter, r *http.Request) {
